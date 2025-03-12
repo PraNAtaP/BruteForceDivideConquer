da672f76835aa6fc5dd688e1bd74b877edb91ee6
PraNAtaP <pranatapu08@gmail.com>
2025-03-12 11:48:42 +0700

    Commit Update Percobaan 1

--- minggu5/Faktorial.java
+++ minggu5/Faktorial.java
@@ -2,10 +2,12 @@ package minggu5;
 
 public class Faktorial {
 
-    int faktorialBF(int n){
+    int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i;
+        int i = 1;
+        while (i <= n) {
+            fakto *= i;
+            i++;
         }
         return fakto;
     }
